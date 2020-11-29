baa10cb2b15424ba63a680cfdbde9cb3db0eb905
Johnny Marshall <the.mighty.git@gmail.com>
2020-11-29 17:30:16 +0000

    try and emulate wheel

--- internal/input/update_touch.go
+++ internal/input/update_touch.go
@@ -16,28 +16,31 @@ var (
 // Update updates the input system. This is called by the UI.
 func Update() {
 	LeftMouseButtonPressed = false
-	touchScrolling = false
 
 	touches := ebiten.TouchIDs()
 	if len(touches) == 1 {
 		// first touchs emulates mouse and left button
 		CursorX, CursorY = ebiten.TouchPosition(touches[0])
 		LeftMouseButtonPressed = true
+
+		touchScrolling = false
 	} else if len(touches) == 2 {
 		// two fingers for scroll
 		if !touchScrolling {
 			// start of scroll touch
-			lastTouchScrollPosX, lastTouchScrollPosY = ebiten.TouchPosition(touches[1])
+			lastTouchScrollPosX, lastTouchScrollPosY = ebiten.TouchPosition(touches[0])
 			log.Println("start scrolling", WheelX, WheelY)
 		} else {
 			// continue scrolling
-			touchScrollPosX, touchScrollPosY := ebiten.TouchPosition(touches[1])
+			touchScrollPosX, touchScrollPosY := ebiten.TouchPosition(touches[0])
 			WheelX += float64(touchScrollPosX - lastTouchScrollPosX)
 			WheelY += float64(touchScrollPosY - lastTouchScrollPosY)
 			log.Println("continue scrolling", WheelX, WheelY)
 			lastTouchScrollPosX, lastTouchScrollPosY = touchScrollPosX, touchScrollPosY
 		}
 		touchScrolling = true
+	} else {
+		touchScrolling = false
 	}
 
 	AnyKeyPressed = false
